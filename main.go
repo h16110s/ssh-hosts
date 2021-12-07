031db38c6ce0eb6fe46f1ca281a7edc7156ba2c5
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2021-12-07 23:57:15 +0900

    Refactor - Use camelCase to follow golang coding rule

--- main.go
+++ main.go
@@ -13,13 +13,13 @@ import (
 const SKIP_LINE = "__SKIP_LINE__"
 
 type Host struct {
-	num       int
-	host      string
-	host_name string
+	num      int
+	host     string
+	hostName string
 }
 
 func keyValueParser(line []string) (string, string) {
-	b_key := false
+	hasKey := false
 	var key string
 	var value string
 	len := len(line)
@@ -31,16 +31,16 @@ func keyValueParser(line []string) (string, string) {
 	for i := 0; i < len; i++ {
 		if line[i] == "" {
 			continue
-		} else if !b_key {
+		} else if !hasKey {
 			key = line[i]
-			b_key = true
+			hasKey = true
 		} else {
 			value = line[i]
 		}
 	}
 
 	// keyに何も入らなければスキップ
-	if !b_key {
+	if !hasKey {
 		key = SKIP_LINE
 	}
 	return key, value
@@ -50,23 +50,23 @@ func main() {
 	homeDir, _ := os.UserHomeDir()
 	data, _ := os.Open(homeDir + "/.ssh/config")
 
-	var host_list []*Host
+	var hosts []*Host
 
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
-	tmp_host := new(Host)
+	tmpHost := new(Host)
 	for scanner.Scan() {
 		line := strings.Split(scanner.Text(), " ")
 		key, value := keyValueParser(line)
 
 		switch strings.ToUpper(key) {
 		case "HOST":
-			host_list = append(host_list, tmp_host)
-			tmp_host = new(Host)
-			tmp_host.host = value
+			hosts = append(hosts, tmpHost)
+			tmpHost = new(Host)
+			tmpHost.host = value
 			break
 		case "HOSTNAME":
-			tmp_host.host_name = value
+			tmpHost.hostName = value
 			break
 		case SKIP_LINE:
 			break
@@ -75,7 +75,7 @@ func main() {
 		}
 	}
 	// 最後のデータを追加
-	host_list = append(host_list, tmp_host)
+	hosts = append(hosts, tmpHost)
 	fmt.Println("ssh hosts")
 
 	table := tablewriter.NewWriter(os.Stdout)
@@ -84,9 +84,9 @@ func main() {
 	table.SetCenterSeparator("|")
 
 	// 1個目のデータは空データになってる(はず)
-	for _, v := range host_list[1:] {
-		str_list := []string{v.host, v.host_name}
-		table.Append(str_list)
+	for _, v := range hosts[1:] {
+		row := []string{v.host, v.hostName}
+		table.Append(row)
 	}
 	table.Render() // Send output
 }
